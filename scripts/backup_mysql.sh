0827e5bdb0215794237ec50bb1bdcd6af1255fb4
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-18 11:35:19 +0800

    Update backup_mysql.sh

--- scripts/backup_mysql.sh
+++ scripts/backup_mysql.sh
@@ -16,7 +16,7 @@ mkdir -p "$BACKUP_DIR"
 mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$BACKUP_DIR/$DB_NAME-$DATE.sql"
 
 # Check if the backup was successful
-# Any application invoke it shell emit non zero values for unsuccessful program invocation 0 = program run successfull
+# Any application invoke in shell emit non zero values for unsuccessful program invocation 0 = program run successfull
 
 if [ $? -eq 0 ]; then
     echo "Backup successful: $BACKUP_DIR/$DB_NAME-$DATE.sql"
